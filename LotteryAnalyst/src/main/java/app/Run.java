3761a3e7b27c40520e9ecf176725bf5c30930f28
Cthulu <djkolesa@gmail.com>
2019-02-03 21:15:06 -0500

    David Kolesar has added logging

--- LotteryAnalyst/src/main/java/app/Run.java
+++ LotteryAnalyst/src/main/java/app/Run.java
@@ -6,7 +6,7 @@ import com.mycompany.dao.DataRetrievalService;
 
 public class Run {
 
-	public static void main(String args[]) throws IOException {
+	public static void main(String args[]) {
 		DataRetrievalService dataRetrievalService = new DataRetrievalService();
 		dataRetrievalService.scrapePage();
 	}
--- LotteryAnalyst/src/main/java/com/mycompany/dao/DataRetrievalService.java
+++ LotteryAnalyst/src/main/java/com/mycompany/dao/DataRetrievalService.java
@@ -2,52 +2,59 @@ package com.mycompany.dao;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class DataRetrievalService {
+	private static final Logger LOGGER = Logger.getLogger(DataRetrievalService.class.getName());
+
 	int gameCount = 1;
 	int iteration = 1;
 	String textHeader = "";
 
 	/**
-	 * Scrapes table from website to get data and concatenates string to present to the user
+	 * Scrapes table from website to get data and concatenates string to present to
+	 * the user
 	 */
 	public void scrapePage() {
 
 		// Attemtping to connect to page
 		Document document = null;
 		try {
+			LOGGER.log( Level.FINE, "Attempting to connect to PA Lottery page");
 			document = Jsoup
-					.connect("https://www.palottery.state.pa.us/scratch-offs/Print-Scratch-Offs.aspx?gametype=Remaining")
+					.connect(
+							"https://www.palottery.state.pa.us/scratch-offs/Print-Scratch-Offs.aspx?gametype=Remaining")
 					.get();
 		} catch (IOException e) {
-			e.printStackTrace();
+			 LOGGER.log( Level.SEVERE, e.toString(), "Unable to connect to lottery page." );
 		}
 		System.out.println(document.title());
 
 		Element table = document.select("table").first();
 		Iterator<Element> iterator = table.select("td").iterator();
 		while (iterator.hasNext()) {
-			
+
 			System.out.println("");
 			System.out.println("Game # " + gameCount);
 			System.out.println("");
-			
+
 			setHeader(iteration);
 			System.out.println(textHeader + " : " + iterator.next().text());
-			iteration = iteration+= 1;
+			iteration = iteration += 1;
 			setHeader(iteration);
 			System.out.println(textHeader + " : " + iterator.next().text());
-			iteration = iteration+= 1;
+			iteration = iteration += 1;
 			setHeader(iteration);
 			System.out.println(textHeader + " : " + iterator.next().text());
-			iteration = iteration+= 1;
+			iteration = iteration += 1;
 			setHeader(iteration);
 			System.out.println(textHeader + " : " + iterator.next().text());
-			iteration = iteration+= 1;
+			iteration = iteration += 1;
 			setHeader(iteration);
 			System.out.println(textHeader + " : " + iterator.next().text());
 			iteration = 1;
